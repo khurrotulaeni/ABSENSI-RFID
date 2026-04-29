86a4e872f528445b48b596ff0ce71dbaef1a5d63
khurrotulaeni <qurrotulaeni281@gmail.com>
2026-04-29 11:40:46 +0700

    Project pemkom

---
+++ src/main/java/com/mycompany/mavenproject1/RFID.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author organizer
+ */
+public class RFID {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
