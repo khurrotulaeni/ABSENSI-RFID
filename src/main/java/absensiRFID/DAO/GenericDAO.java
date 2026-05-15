8a5c2bb9f864b8e4a3f605500498e1656c1072dd
khurrotulaeni <qurrotulaeni281@gmail.com'>
2026-05-15 17:01:41 +0700

    Buat LoginPage dan AdminPage

--- src/main/java/absensiRFID/DAO/GenericDAO.java
+++ src/main/java/absensiRFID/DAO/GenericDAO.java
@@ -4,20 +4,16 @@
  */
 package absensiRFID.DAO;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import com.mycompany.rfid_absensi_siswa.object.MongoManager;
 import org.bson.conversions.Bson;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author titosukamenabung
- */
 public class GenericDAO<T> implements BaseDAO<T> {
     private final MongoCollection<T> collection;
     private final Class<T> clazz;
 
-    // Konstruktor menerima nama koleksi dan kelas entitas untuk mapping otomatis
     public GenericDAO(String collectionName, Class<T> clazz) {
         this.clazz = clazz;
         this.collection = MongoManager.getDatabase().getCollection(collectionName, clazz);
@@ -25,29 +21,37 @@ public class GenericDAO<T> implements BaseDAO<T> {
 
     @Override
     public void save(T entity) {
+        // Mengubah objek Java (Entity) menjadi Document MongoDB
         collection.insertOne(entity);
     }
 
     @Override
-    public void update(Bson filter, T entity) {
+    public void update(String idField, String idValue, T entity) {
+        Bson filter = Filters.eq(idField, idValue);
         collection.replaceOne(filter, entity);
     }
-
+    
     @Override
-    public void delete(Bson filter) {
+    public void delete(String idField, String idValue) {
+        Bson filter = Filters.eq(idField, idValue);
         collection.deleteOne(filter);
     }
-
+    
     @Override
-    public List<T> findAll() {
+    public List<T> getAll() {
         return collection.find().into(new ArrayList<>());
     }
 
     @Override
-    public T findOne(Bson filter) {
-        return collection.find(filter).first();
+    public List<T> findAll(){
+        return collection.find().into(new ArrayList<>());
     }
-
+    
+     @Override
+     public T findOne (Bson filter) {
+         return collection.find(filter).first();
+     }
+     
     @Override
     public List<T> findMany(Bson filter) {
         return collection.find(filter).into(new ArrayList<>());
